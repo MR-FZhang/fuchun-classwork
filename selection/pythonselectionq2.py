9af25e0bd63b08e19f01c778d45fc0670751d702
MR-FZhang <90248676+MR-FZhang@users.noreply.github.com>
2021-10-07 09:51:27 +0100

    Update pythonselectionq2.py

--- selection/pythonselectionq2.py
+++ selection/pythonselectionq2.py
@@ -1,1 +1,17 @@
+amount = float(input("the value of order"))
+if (amount>= 15): 
+    print("enter code 1 for next day delivery", "standard enter code 2")
+method1 = str(input())
+if (method1):="code 1":
+    print(int(0),amount+int(0))
+
+else: print (5,amount+int(5)) 
+
+if (amount< 15):
+   print ("enter code 1 for next day delivery", "standard enter code 2")
+method1= str(input())
+if (method1=="code 1"): 
+    print (3.5,amount+float(3.5))
+
+else: print (5,amount+int(5))
 #end if
